c1579b4399a6c5e49caa8b743cf88ccd625988cf
Ian Cahn <iancahn@live.com>
2022-08-28 18:53:52 +0200

    Update Api.jsx

--- src/components/Api.jsx
+++ src/components/Api.jsx
@@ -10,7 +10,8 @@ function fetchData() {
             // handle success
             console.log(data);
             // return data;
-            return JSON.stringify(data);
+            // return JSON.stringify(data);
+            return data;
         })
         .catch(error => {
             // handle error
@@ -18,21 +19,31 @@ function fetchData() {
         });
 }
 
-
+const getFullUserName = (userInfo) => {
+    const { name: { first, last } } = userInfo;
+    return `${first} ${last}`;
+}
 
 function Api() {
     const [randomUser, setRandomUser] = useState('');
+    const [userInfos, setUserInfos] = useState([]);
 
     useEffect(() => {
         fetchData().then(randomData => {
-            setRandomUser(randomData);
+            setRandomUser(JSON.stringify(randomData) || "No user data found.");
+            setUserInfos(randomData.results);
+            console.log("UserInfos " + userInfos);
         });
 
     }, []);
+    // const userEmail = randomUser.results[0].email;
 
     return (
         <div>
             <h2>{randomUser}</h2>
+            {userInfos.map((userInfo) => {
+                return <p>{getFullUserName(userInfo)}</p>
+            })}
         </div>
     )
 }
